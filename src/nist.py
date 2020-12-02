8f60d04ef0eba7ae08f39701a4348da44f76bded
Juan Velazquez <velazquezrj2012@licifug.ugto.mx>
2020-12-02 12:55:56 -0600

    temperature_correction_newFunction

--- src/nist.py
+++ src/nist.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import interpolate
 
 def get_reference_NIST_values(serial_number):
     '''get_reference_NIST_values
@@ -67,3 +68,50 @@ def get_NIST_ids():
         'T1 - NIST sphere 14',
     ]
     return ids
+
+def temperature_correction(input_temperature):
+    #Dictionary of data
+    phantom_v2 = {
+        '14': {'16': '21.94', '18': '21.62','20': '21.44', '22': '21.28', '24': '21.26', '26': '21.31'},
+        '13': {'16': '31.05', '18': '30.65','20': '30.40', '22': '30.27', '24': '30.25', '26': '30.31'},
+        '12': {'16': '43.79', '18': '43.24','20': '42.89', '22': '42.72', '24': '42.70', '26': '42.80'},
+        '11': {'16': '61.49', '18': '60.70','20': '60.21', '22': '59.97', '24': '60.00', '26': '60.17'},
+        '10': {'16': '87.47', '18': '86.41','20': '85.75', '22': '85.03', '24': '85.01', '26': '85.28'},
+        '9': {'16': '122.99', '18': '121.79','20': '121.08', '22': '120.80', '24': '120.90', '26': '121.34'},
+        '8': {'16': '177.68', '18': '175.94','20': '174.95', '22': '174.59', '24': '174.78', '26': '175.48'},
+        '7': {'16': '243.77', '18': '241.84','20': '240.86', '22': '240.75', '24': '241.31', '26': '242.45'},
+        '6': {'16': '343.00', '18': '341.53','20': '341.58', '22': '342.58', '24': '344.23', '26': '346.67'},
+        '5': {'16': '483.91', '18': '482.91','20': '484.97', '22': '486.92', '24': '490.24', '26': '494.55'},
+        '4': {'16': '675.07', '18': '686.88','20': '690.08', '22': '695.01', '24': '701.06', '26': '709.48'},
+        '3': {'16': '950.71', '18': '963.56','20': '987.27', '22': '1000.81', '24': '1015.70', '26': '1030.78'},
+        '2': {'16': '1274.07', '18': '1317.71','20': '1330.16', '22': '1355.29', '24': '1367.79', '26': '1395.94'},
+        '1': {'16': '1766.68', '18': '1830.34','20': '1883.97', '22': '1937.34', '24': '1987.50', '26': '2066.95'}
+        };
+    
+    ##Get keys and values of dictionary to construct a 2D array##
+    #Get dictionary keys as lists
+    sphere = list(phantom_v2.keys());
+    temperature = list(phantom_v2['1'].keys());
+
+    #Define array (data2fit) to store data to be fitted
+    data2fit = np.empty([len(temperature),len(sphere)+1]);
+    #Fill data2fit array
+    row = 0;
+    for i in temperature:
+        row = row + 1;
+        data2fit[row-1,0] = int(i);
+        for j in sphere:
+            T1_value = float(phantom_v2[j][i]);
+            data2fit[row-1,int(j)] = T1_value;
+        
+
+    ##Code for Temperature Correction: Interpolation##
+    estimatedT1_values = np.empty([len(sphere)]);
+    outDic_T1values = {};
+    #Interpolations
+    for k in range(len(sphere)):
+        f_cubic = interpolate.interp1d(data2fit[:,0], data2fit[:,k+1], kind='cubic');
+        estimatedT1_values[k] = f_cubic(input_temperature);
+        outDic_T1values[k+1] = estimatedT1_values[k];
+        
+    return outDic_T1values, estimatedT1_values;
